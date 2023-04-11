a8b23ccbf37478e2e3bae993878cfb518e9789d1
Jin Cheng <jcheng@tuebingen.mpg.de>
2023-04-11 14:59:24 +0200

    test

--- solo_legged_gym/utils/helpers.py
+++ solo_legged_gym/utils/helpers.py
@@ -242,6 +242,7 @@ def merge_config_args_into_cmd_line(args):
         else:
             sys.argv.append(f"--{k}={v}")
 
+
 def update_cfgs_from_dict(env_cfg, train_cfg, update_cfg):
     update_class_from_dict(env_cfg, update_cfg["legged_gym"]["env_cfg"])
     update_class_from_dict(train_cfg, update_cfg["legged_gym"]["train_cfg"])
