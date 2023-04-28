f279541fc41845dca8be2a8a5f99ad783aee42b7
Jin Cheng <jicheng@ethz.ch>
2023-04-28 14:20:36 +0200

    well before really works

--- solo_legged_gym/envs/solo12_vanilla/solo12_vanilla.py
+++ solo_legged_gym/envs/solo12_vanilla/solo12_vanilla.py
@@ -413,7 +413,7 @@ class Solo12Vanilla(BaseTask):
 
     def _reward_joint_default(self, sigma):
         joint_deviation = torch.norm(self.dof_pos - self.default_dof_pos, p=2, dim=1)
-        return torch.clip(torch.exp(-torch.square(joint_deviation / sigma)), min=None, max=0.9) / 0.9
+        return torch.clip(torch.exp(-torch.square(joint_deviation / sigma)), min=None, max=0.7) / 0.7
 
     def _reward_joint_targets_rate(self, sigma):
         return torch.exp(-torch.square(self.joint_targets_rate / sigma))
