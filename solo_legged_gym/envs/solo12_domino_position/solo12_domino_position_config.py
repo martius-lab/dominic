a8bc445b7615e38cb4f664cd0bc9eadfe13ada73
Jin Cheng <jicheng@ethz.ch>
2023-07-19 10:01:00 +0200

    conditioned

--- solo_legged_gym/envs/solo12_domino_position/solo12_domino_position_config.py
+++ solo_legged_gym/envs/solo12_domino_position/solo12_domino_position_config.py
@@ -259,7 +259,7 @@ class Solo12DOMINOPositionTrainCfg:
         # logging
         save_interval = 50  # check for potential saves every this many iterations
         experiment_name = 'solo12_domino_position'
-        run_name = 'new_terrain2'
+        run_name = 'conditioned'
 
         # load
         load_run = -1  # -1 = last run
