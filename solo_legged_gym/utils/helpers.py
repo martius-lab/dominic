19c618bfa12b004da45d8570e808e2d420dea612
Jin Cheng <jcheng@tuebingen.mpg.de>
2023-09-05 17:43:06 +0200

    minor

--- solo_legged_gym/utils/helpers.py
+++ solo_legged_gym/utils/helpers.py
@@ -29,12 +29,6 @@ def get_args() -> argparse.Namespace:
             "default": False,
             "help": "Turn on Weights and Bias writer",
         },
-        {
-            "name": "--dr",
-            "action": "store_false",
-            "default": True,
-            "help": "Disable recording gifs",
-        },
     ]
     # parse arguments
     args = gymutil.parse_arguments(description="RL Policy using IsaacGym", custom_parameters=custom_parameters)
@@ -143,8 +137,6 @@ def update_env_cfg_from_args(env_cfg, args):
 def update_train_cfg_from_args(train_cfg, args):
     if args.w is not None:
         train_cfg.runner.wandb = args.w
-    if args.dr is not None:
-        train_cfg.runner.record_gif = args.dr
     return train_cfg
 
 
