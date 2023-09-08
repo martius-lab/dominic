ac9204f7e93c82b74b265d95d5f7bd893dacb03e
Jin Cheng <jcheng@tuebingen.mpg.de>
2023-09-08 16:58:23 +0200

    added face towards reward

--- solo_legged_gym/envs/solo12_domino_position/solo12_domino_position.py
+++ solo_legged_gym/envs/solo12_domino_position/solo12_domino_position.py
@@ -879,6 +879,12 @@ class Solo12DOMINOPosition(BaseTask):
         norm_rew = torch.sum(target_pos_in_base_normalized * base_lin_vel_normalized, dim=-1) / 2 + 0.5
         return torch.clip(norm_rew, min=None, max=sigma) / sigma
 
+    def _reward_move_face(self, sigma):  # face towards target
+        target_pos_in_base_normalized = self.commands_in_base[:, 0:3] / (
+                torch.norm(self.commands_in_base[:, 0:3], dim=-1, keepdim=True) + 1e-8)
+        base_face_direction = torch.tensor([1, 0, 0], dtype=torch.float, device=self.device).repeat(self.num_envs, 1)
+        norm_rew = torch.sum(target_pos_in_base_normalized * base_face_direction, dim=-1) / 2 + 0.5
+        return torch.clip(norm_rew, min=None, max=sigma) / sigma
 
     # def _reward_feet_slip(self, sigma):  # feet should not slip
     #     feet_low = self.ee_global[:, :, 2] < self.ee_terrain_heights + sigma[0]
--- solo_legged_gym/envs/solo12_domino_position/solo12_domino_position_config.py
+++ solo_legged_gym/envs/solo12_domino_position/solo12_domino_position_config.py
@@ -154,15 +154,17 @@ class Solo12DOMINOPositionEnvCfg(BaseEnvCfg):
             yawi = "[0, [1.0, 0.25]]"  # scale of the error, check distance
 
             joint_targets_rate = "[1, 1.0]"
-            # feet_acc = "[1, 1000]"
             contact = "[1, 1]"
             feet_contact = "[1, 1]"
             stall_pos = "[1, [0.3, 0.25, 0.1]]"  # minimal vel, distance, sigma
             gravity = "[1, 1.0]"
-            # joint_default = "[1, 10.0]"
             torques = "[1, 300]"
+            # joint_default = "[1, 10.0]"
+            # feet_acc = "[1, 1000]"
 
             move_towards = "[2, 1.0]"  # clip/scale
+            move_face = "[2, 1.0]"  # clip/scale
+
             # feet_slip = "[2, [0.04, 0.1, 0.4]]"  # target height, sigma, sigma+
 
             # ang_xy = "[2, 0.1]"
@@ -285,7 +287,7 @@ class Solo12DOMINOPositionTrainCfg:
         # logging
         save_interval = 50  # check for potential saves every this many iterations
         experiment_name = 'solo12_domino_position'
-        run_name = 'blm'
+        run_name = 'blm_face'
 
         # cluster
         restart_interval = 10000  # not working now on cluster, segmentation fault
